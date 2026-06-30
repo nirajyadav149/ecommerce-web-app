538a1fb88a7f33451d3a72e420494a5afe36ca0a
nirajyadav149 <nirajyadav36149@gmail.com>
2026-06-30 10:25:45 +0530

    Add click event to buttons for cart alert

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,7 @@
+const buttons = document.querySelectorAll("button");
 
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        alert("Product added to cart!");
+    });
+});
